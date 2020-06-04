edb11e6b5284e2711b4b88ea2cae9d7b480bed80
fede <fedes_112@hotmail.com>
2020-06-04 19:45:29 -0300

    conectadas las especies lideres del back

--- my-app/src/components/leaderBoard/especiesLideres/especiesLeaderboard.jsx
+++ my-app/src/components/leaderBoard/especiesLideres/especiesLeaderboard.jsx
@@ -1,76 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Image, Table, Row, Col } from "react-bootstrap";
 import Persona from "./resources/persona.png";
 import { connect } from "react-redux";
 
-const mockedData = [
-  {
-    especie: "Paperas",
-    patogeno: "Virus",
-    cantidadDeVectoresInfectados: 2300,
-    ubicacionMasInfectada: "Varela",
-    esPandemia: "No",
-  },
-  {
-    especie: "Tuberculosis",
-    patogeno: "Bacteria",
-    cantidadDeVectoresInfectados: 2100,
-    ubicacionMasInfectada: "Caballito",
-    esPandemia: "Si",
-  },
-  {
-    especie: "Chiripioca",
-    patogeno: "Bacteria",
-    cantidadDeVectoresInfectados: 1830,
-    ubicacionMasInfectada: "Glew",
-    esPandemia: "Si",
-  },
-  {
-    especie: "Zapatarata",
-    patogeno: "Parasito",
-    cantidadDeVectoresInfectados: 1700,
-    ubicacionMasInfectada: "Quilmes",
-    esPandemia: "No",
-  },
-  {
-    especie: "Zomboid",
-    patogeno: "Parasito del nuevo mundo",
-    cantidadDeVectoresInfectados: 1300,
-    ubicacionMasInfectada: "Villa Luro",
-    esPandemia: "Si",
-  },
-  {
-    especie: "Fiebre Amarilla",
-    patogeno: "Virus",
-    cantidadDeVectoresInfectados: 834,
-    ubicacionMasInfectada: "Palermo",
-    esPandemia: "No",
-  },
-  {
-    especie: "Ritmo rojo",
-    patogeno: "Virus",
-    cantidadDeVectoresInfectados: 712,
-    ubicacionMasInfectada: "Ghana",
-    esPandemia: "No",
-  },
-
-  {
-    especie: "Dengue",
-    patogeno: "Virus",
-    cantidadDeVectoresInfectados: 637,
-    ubicacionMasInfectada: "Ranela",
-    esPandemia: "Si",
-  },
-];
-
 const EspecieRow = ({ data }) => {
   return (
     <tr>
-      <td>{data.especie}</td>
-      <td>{data.patogeno}</td>
-      <td>{data.cantidadDeVectoresInfectados}</td>
-      <td>{data.ubicacionMasInfectada}</td>
-      <td>{data.esPandemia}</td>
+      <td>{data.especie_nombre}</td>
+      <td>{data.especie_patogeno}</td>
+      <td>{data.cantidadInfectados}</td>
+      <td>{data.esPandemia === false ? "NO" : "SI"}</td>
     </tr>
   );
 };
@@ -82,14 +21,11 @@ const RowVacia = () => {
       <td></td>
       <td></td>
       <td></td>
-      <td></td>
     </tr>
   );
 };
 
 const EspeciesLeaderboard = (especies_lideres) => {
-  console.log("ACAAAAAAAAAAAAAAAAAAAAAA");
-  console.log(especies_lideres);
   return (
     <Row className="pt-0">
       <Col md="2" className="pr-0" style={{ paddingLeft: "10px" }}>
@@ -111,13 +47,16 @@ const EspeciesLeaderboard = (especies_lideres) => {
               <th>Especie</th>
               <th>Patogeno</th>
               <th>Vectores infectados</th>
-              <th>Ubicacion mas infectada</th>
               <th>Es Pandemia</th>
             </tr>
           </thead>
           <tbody>
             {[...Array(10)].map((_, i) =>
-              mockedData[i] ? <EspecieRow data={mockedData[i]} /> : <RowVacia />
+              especies_lideres.especies_lideres[i] ? (
+                <EspecieRow data={especies_lideres.especies_lideres[i]} />
+              ) : (
+                <RowVacia />
+              )
             )}
           </tbody>
         </Table>
